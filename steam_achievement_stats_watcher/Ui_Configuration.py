2371c339657876f49633361b9b468886f2818a3d
Ryan Gooler <cheetahmorph@gmail.com>
2019-06-06 01:30:45 -0500

    Commiting kinda working stuff

--- steam_achievement_stats_watcher/Ui_Configuration.py
+++ steam_achievement_stats_watcher/Ui_Configuration.py
@@ -2,12 +2,12 @@ import sys
 from PyQt5.QtWidgets import QDialog
 import ui
 
-class MyAPP(QDialog):
+class Ui_Configuration(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = ui.Ui_Configuration()
         self.ui.setupUi(self)
-        self.ui.Action.clicked.connect(self.configButtonBox)
+        #self.ui.Action.clicked.connect(self.configButtonBox)
         self.show()
     def message(self):
         #self.ui.Print.setText("Hello " + self.ui.Input1.text() + ", your age is " + self.ui.Input2.text())
