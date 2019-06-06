2bdc82a1c8172da30e0d8eb19e05ca3cdec7f10c
Ryan Gooler <cheetahmorph@gmail.com>
2019-06-06 11:22:29 -0500

    Basic dialogs wired together

--- steam_achievement_stats_watcher/Main.py
+++ steam_achievement_stats_watcher/Main.py
@@ -1,23 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QDialog, qApp
+from PyQt5.QtWidgets import QDialog, qApp
 import steam_achievement_stats_watcher
 import ui
 
 
-class Main(QMainWindow):
+class Main(QDialog):
     def __init__(self):
         super().__init__()
-        self.ui = ui.Ui_Main()
-        self.ui.setupUi(self)
+        self.main = ui.Ui_Main()
+        self.main.setupUi(self)
         # Enable menu buttons
-        self.ui.AddGame.triggered.connect(qApp.quit)
-        self.ui.Configure.triggered.connect(self.config_window)
+        self.main.AddGameButton.pressed.connect(self.addgame_window)
+        self.main.ConfigureButton.pressed.connect(self.config_window)
         self.show()
 
     def config_window(self):
-        print("config_window")
         widget = QDialog(self)
-        ui = ui.Ui_Configuration()
-        ui.setupUi(widget)
+        dialog = ui.Ui_Configuration()
+        dialog.setupUi(widget)
+        widget.exec_()
+        widget.show()
+
+    def addgame_window(self):
+        widget = QDialog(self)
+        dialog = ui.Ui_AddGame()
+        dialog.setupUi(widget)
         widget.exec_()
         widget.show()
--- ui/Ui_AddGame.py
+++ ui/Ui_AddGame.py
@@ -11,25 +11,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_AddGame(object):
     def setupUi(self, AddGame):
         AddGame.setObjectName("AddGame")
-        AddGame.resize(670, 381)
+        AddGame.resize(400, 300)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("../icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         AddGame.setWindowIcon(icon)
-        self.centralwidget = QtWidgets.QWidget(AddGame)
-        self.centralwidget.setObjectName("centralwidget")
-        AddGame.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(AddGame)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 670, 21))
-        self.menubar.setObjectName("menubar")
-        AddGame.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(AddGame)
-        self.statusbar.setObjectName("statusbar")
-        AddGame.setStatusBar(self.statusbar)
 
         self.retranslateUi(AddGame)
         QtCore.QMetaObject.connectSlotsByName(AddGame)
 
     def retranslateUi(self, AddGame):
         _translate = QtCore.QCoreApplication.translate
-        AddGame.setWindowTitle(_translate("AddGame", "Add Game/cheevos"))
+        AddGame.setWindowTitle(_translate("AddGame", "Add Game"))
 
