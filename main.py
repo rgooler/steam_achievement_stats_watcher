0956cf7e5a4a055fc60e27ca5483134c1e988d98
Ryan Gooler <cheetahmorph@gmail.com>
2019-06-06 01:06:53 -0500

    Update tag again

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
+import steam_achievement_stats_watcher
+from PyQt5.QtWidgets import QApplication
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
-import ui
-
-class MyAPP(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.ui = ui.Ui_Configuration()
-        self.ui.setupUi(self)
-        #self.ui.Action.clicked.connect(self.message)
-        self.show()
-    def message(self):
-        #self.ui.Print.setText("Hello " + self.ui.Input1.text() + ", your age is " + self.ui.Input2.text())
-        pass
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
-    w = MyAPP()
+    w = steam_achievement_stats_watcher.Ui_Configuration()
     w.show()
     sys.exit(app.exec_())
---
+++ steam_achievement_stats_watcher/__init__.py
@@ -0,0 +1,2 @@
+from steam_achievement_stats_watcher.Ui_Configuration import Ui_Configuration
+__all__ = ["Ui_Configuration"]
